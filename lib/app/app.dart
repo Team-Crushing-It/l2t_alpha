5a63af4c7c9417be2d748e2682db2ae999597399
Gene <45580241+Gene-Dana@users.noreply.github.com>
2021-03-05 17:21:25 -0500

    Update app.dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,21 +1,11 @@
-// Copyright (c) 2021, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:l2t_alpha/l10n/l10n.dart';
 import 'package:flow_builder/flow_builder.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'package:l2t_alpha/authentication/authentication.dart';
-
 import 'package:authentication_repository/authentication_repository.dart';
-
-import 'package:l2t_alpha/homepage_auth/homepage_auth.dart';
+import 'package:l2t_alpha/authentication/authentication.dart';
 import 'package:l2t_alpha/homepage_unauth/homepage_unauth.dart';
 import 'package:l2t_alpha/counter/counter.dart';
 
