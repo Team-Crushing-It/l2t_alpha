1de0cf9fb11adb9a8793ccb098215d9acbea5306
Gene <45580241+Gene-Dana@users.noreply.github.com>
2021-03-10 12:48:06 -0500

    added back in tests

---
+++ test/app_test.dart
@@ -0,0 +1,21 @@
+// Copyright (c) 2021, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:l2t_alpha/app/app.dart';
+import 'package:l2t_alpha/counter/counter.dart';
+import 'package:authentication_repository/authentication_repository.dart';
+
+void main() {
+  group('App', () {
+    testWidgets('renders CounterPage', (tester) async {
+      await tester.pumpWidget(
+          App(authenticationRepository: AuthenticationRepository()));
+      expect(find.byType(CounterPage), findsOneWidget);
+    });
+  });
+}
