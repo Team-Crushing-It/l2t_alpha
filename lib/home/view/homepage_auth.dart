4d0277dd72de225a6545c70feb0c1e39a3b02a53
Gene <45580241+Gene-Dana@users.noreply.github.com>
2021-03-16 14:04:13 -0400

    update :D

--- lib/home/view/homepage_auth.dart
+++ lib/home/view/homepage_auth.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:l2t_alpha/navigation/navbar/navbar.dart';
+import 'package:l2t_alpha/authentication/authentication.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePageAuth extends StatelessWidget {
   static Page page() => MaterialPage<void>(child: HomePageAuth());
 
   @override
   Widget build(BuildContext context) {
+    final user = context.watch<AuthenticationBloc>().state.user;
+
     return Scaffold(
       appBar: NavBar(),
-      body: const Center(child: Text('Home Authenticated')),
+      body: Center(child: Text(user.name.toString())),
     );
   }
 }
